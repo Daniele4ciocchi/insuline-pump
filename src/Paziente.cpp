ef08af29a9af6fa872d12ad1320da2361cbb2017
= <=>
2024-05-21 12:18:01 +0200

    aggiunta la struttura del paziente

---
+++ src/Paziente.cpp
@@ -0,0 +1,5 @@
+//
+// Created by daniele on 21/05/24.
+//
+
+#include "Paziente.h"
---
+++ src/Paziente.h
@@ -0,0 +1,14 @@
+//
+// Created by daniele on 21/05/24.
+//
+
+#ifndef INSULINE_PUMP_PAZIENTE_H
+#define INSULINE_PUMP_PAZIENTE_H
+
+
+class Paziente {
+
+};
+
+
+#endif //INSULINE_PUMP_PAZIENTE_H
