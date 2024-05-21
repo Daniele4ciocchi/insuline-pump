46e138b3dde3c7fcb03228e02caaded5160fbf6b
= <=>
2024-05-21 12:39:12 +0200

    update paziente

--- src/Paziente.cpp
+++ src/Paziente.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "Paziente.h"
+
+// Implementazione del costruttore
+Paziente::Paziente(double glucosioIniziale, double insulinaIniziale) {
+    glucosio = glucosioIniziale;
+    insulina = insulinaIniziale;
+}
+
+// Implementazione dei metodi getter
+double Paziente::getGlucosio() const {
+    return glucosio;
+}
+
+double Paziente::getInsulina() const {
+    return insulina;
+}
+
+// Implementazione dei metodi per simulare l'assunzione di cibo e l'iniezione di insulina
+void Paziente::assumeCibo(double quantita) {
+    // implementazione...
+}
+
+void Paziente::iniettaInsulina(double dose) {
+    // implementazione...
+}
+
+// Implementazione del metodo per aggiornare lo stato del paziente
+void Paziente::aggiornaStato() {
+    // implementazione...
+}
--- src/Paziente.h
+++ src/Paziente.h
@@ -7,7 +7,24 @@
 
 
 class Paziente {
+private:
+    double glucosio; // Concentrazione di glucosio nel sangue
+    double insulina; // Dose di insulina
 
+public:
+    // Costruttore
+    Paziente(double glucosioIniziale, double insulinaIniziale) : glucosio(glucosioIniziale), insulina(insulinaIniziale) {}
+
+    // Metodi getter per ottenere i valori correnti di glucosio e insulina
+    double getGlucosio() const { return glucosio; }
+    double getInsulina() const { return insulina; }
+
+    // Metodi per simulare l'assunzione di cibo e l'iniezione di insulina
+    void assumeCibo(double quantita) { /* implementazione... */ }
+    void iniettaInsulina(double dose) { /* implementazione... */ }
+
+    // Metodo per aggiornare lo stato del paziente (es. calcolare il nuovo livello di glucosio)
+    void aggiornaStato() { /* implementazione... */ }
 };
 
 
